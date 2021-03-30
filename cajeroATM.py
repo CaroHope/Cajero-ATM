1ee64522a12f60faa3b361ab6663a05b3ef5bd38
Carolina <evecarojorge@gmail.com>
2021-03-29 22:30:35 -0400

    Cajero ATM listo

--- cajeroATM.py
+++ cajeroATM.py
@@ -1,7 +1,7 @@
 '''
 Descripcion: Programa que simula un Cajero Automatico dispensador
-             de efectivo (ATM). El usuario puede ingresar y realizar
-             tres funciones contenidas en el progr5ama Back-Office.
+             de efectivo (ATM). El usuario puede logearse y realizar
+             tres funciones contenidas en el programa Back-Office.
              *Inicialice este Programa para usar el cajero.
 
 Autor: Evelyn Carolina Jorge
@@ -9,11 +9,11 @@ Matricula: 20-0084
 
 Segundo parcial
 '''
+import time
 
-nombre = ["Maria Perez", "Pedro Martin", "Mari Mendoza"]
-pin = [1234,4321,9876]
-
-
+nombre = ["Maria Perez", "Pedro Martin", "Mari Mendoza", "Kelly Molly"]
+pin = [1234,4321,9876, 7654]
+nocuenta= [20091302,38103827,1083728,92836234]
 
 contru = 0
 x= 0
@@ -22,8 +22,17 @@ indice = 0
 
 #Login=========================
 
+print("< C A J E R O >")
+print("Bienvenido a su cajero favorito,")
+time.sleep(1)
 while contru == 0 and x <=2:
-    pinlo = int(input("Ingrese su Pin: "))
+    pinlo = input("Escriba su Pin para ingresar: ")
+    try:
+        pinlo = int(pinlo)
+    except ValueError:
+        print("Escriba datos enteros.")
+        time.sleep(0.5)
+        exit
     for f in range (3):
         l = pin[f]
         if pinlo == l:
@@ -50,7 +59,12 @@ if contru == 1:
         print("Consultar balance (1)")
         print("Retiro de Efectivo(2)")
         print("Depositar (3)")
-        op = int(input("Elija una Opcion: "))
+        op = input("Elija una Opcion: ")
+        try:
+            op = int(op)
+        except ValueError:
+            print("Escriba datos enteros.")
+            time.sleep(0.5)
         
         if op==1 or op==2 or op==3:
             y = y+1
@@ -69,8 +83,8 @@ else:
 
     
 print("="*5)
-#desea ir al menu
-#desea salir
+print("Bye")
+print(" ")
 
 
 
--- funcionesBack.py
+++ funcionesBack.py
@@ -9,14 +9,17 @@ Matricula: 20-0084
 
 Segundo parcial
 '''
+import time
+
 #balances por Defalut:
-balancee = [500, 100, 2000]
+balancee = [500, 100, 2000,100]
 
 
 def Consultar_Balance (indice):
     mostrarbalance = balancee[indice]
+    time.sleep(0.5)
     print("Su balance es:", mostrarbalance)
-
+    
 
 def Retiro (indice):
     monto = int(input("Ingrese Monto a Retirar: "))
@@ -42,9 +45,13 @@ def Retiro (indice):
 
         de5 = int(monto/5)
         monto = int(monto%5)
+
+        de1 = int(monto/1)
+        monto = int(monto%1)
         
+        time.sleep(0.5)
         print("Efectivo retirado de: ")
-        print(" De Mil:",deMil, "\n","De Cien:", deCien," \n", "De Cincuenta:", deCincuenta," \n", "De 25:", de25, "\n", "De 10:", de10,"\n", "De 5:", de5,"\n")
+        print(" De Mil:",deMil, "\n","De Cien:", deCien," \n", "De Cincuenta:", deCincuenta," \n", "De 25:", de25, "\n", "De 10:", de10,"\n", "De 5:", de5,"\n","De 1:", de1,"\n")
         print(" ")
         print("Su nuevo monto es: ", reti)
 
@@ -55,5 +62,6 @@ def Retiro (indice):
 def Deposito (indice):
     depo = int(input("Ingrese Monto a Depositar:"))
     balancee[indice] = depo + balancee[indice]
+    time.sleep(0.5)
     print("Su cuenta ha sido actualizada.")
     print("Su nuevo Balance es de: ", balancee[indice])
